78f22f277e8913decaf6cf18620b25673be84848
Jadeiin <github@qunn.eu>
2023-09-22 12:52:19 +0800

    Format code

--- src/vec3.h
+++ src/vec3.h
@@ -1,15 +1,5 @@
 #ifndef VEC3_H
 #define VEC3_H
-//==============================================================================================
-// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all copyright and related and
-// neighboring rights to this software to the public domain worldwide. This software is
-// distributed without any warranty.
-//
-// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
-// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//==============================================================================================
 
 #include <cmath>
 #include <iostream>
@@ -58,6 +48,9 @@ class vec3 {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 
+    /**
+     * @brief 判断向量是否接近零点
+     */
     bool near_zero() const {
         // Return true if the vector is close to zero in all dimensions.
         auto s = 1e-8;
@@ -68,6 +61,13 @@ class vec3 {
         return vec3(random_double(), random_double(), random_double());
     }
 
+    /**
+     * @brief 返回坐标在指定区间内的随机向量
+     *
+     * @param min 最小坐标
+     * @param max 最大坐标
+     * @return vec3 随机向量
+     */
     static vec3 random(double min, double max) {
         return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
     }
@@ -107,22 +107,47 @@ inline vec3 operator/(vec3 v, double t) {
     return (1/t) * v;
 }
 
+/**
+ * @brief 向量点乘
+ *
+ * @param u
+ * @param v
+ * @return double
+ */
 inline double dot(const vec3 &u, const vec3 &v) {
     return u.e[0] * v.e[0]
          + u.e[1] * v.e[1]
          + u.e[2] * v.e[2];
 }
 
+/**
+ * @brief 向量叉乘
+ *
+ * @param u
+ * @param v
+ * @return vec3
+ */
 inline vec3 cross(const vec3 &u, const vec3 &v) {
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
                 u.e[2] * v.e[0] - u.e[0] * v.e[2],
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
+/**
+ * @brief 单位向量化
+ *
+ * @param v
+ * @return vec3 单位向量
+ */
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
+/**
+ * @brief 返回xy坐标系中单位圆内的随机向量
+ *
+ * @return vec3
+ */
 inline vec3 random_in_unit_disk() {
     while (true) {
         auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
@@ -131,6 +156,11 @@ inline vec3 random_in_unit_disk() {
     }
 }
 
+/**
+ * @brief 返回单元球内的随机向量
+ *
+ * @return vec3
+ */
 inline vec3 random_in_unit_sphere() {
     while (true) {
         auto p = vec3::random(-1,1);
@@ -139,10 +169,21 @@ inline vec3 random_in_unit_sphere() {
     }
 }
 
+/**
+ * @brief 返回随机单位向量
+ *
+ * @return vec3
+ */
 inline vec3 random_unit_vector() {
     return unit_vector(random_in_unit_sphere());
 }
 
+/**
+ * @brief 返回位于给定法线方向上的随机半球上的随机向量
+ *
+ * @param normal 用于确定半球方向的法线
+ * @return vec3 位于半球上的随机点,方向同法线
+ */
 inline vec3 random_on_hemisphere(const vec3& normal) {
     vec3 on_unit_sphere = random_unit_vector();
     if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
@@ -151,10 +192,25 @@ inline vec3 random_on_hemisphere(const vec3& normal) {
         return -on_unit_sphere;
 }
 
+/**
+ * @brief 向量反射
+ *
+ * @param v 入射向量
+ * @param n 法线向量
+ * @return vec3 反射向量
+ */
 inline vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2*dot(v,n)*n;
 }
 
+/**
+ * @brief 向量折射
+ *
+ * @param uv 入射单位向量
+ * @param n 法线单位向量
+ * @param etai_over_etat 折射率之比（入射介质的折射率 / 折射介质的折射率）
+ * @return vec3 折射向量
+ */
 inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
     auto cos_theta = fmin(dot(-uv, n), 1.0);
     vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n);
