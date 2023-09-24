0a459dd8fa64049a557f0e096017cfbc4aecf71b
TT2TER <93923763+TT2TER@users.noreply.github.com>
2023-09-24 21:01:45 +0800

    添加了让马煜能看懂这段代码的必要注释

--- src/vec3.h
+++ src/vec3.h
@@ -10,22 +10,30 @@ using std::fabs;
 class vec3 {
   public:
     double e[3];
-
+    //构造函数
     vec3() : e{0,0,0} {}
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
-
+    //返回坐标
     double x() const { return e[0]; }
     double y() const { return e[1]; }
     double z() const { return e[2]; }
 
+    //重载运算符
     vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
     double operator[](int i) const { return e[i]; }
     double& operator[](int i) { return e[i]; }
-
+    
     vec3& operator+=(const vec3 &v) {
         e[0] += v.e[0];
         e[1] += v.e[1];
         e[2] += v.e[2];
+    /*
+    vec3 a(1.0, 2.0, 3.0);
+    vec3 b(4.0, 5.0, 6.0);
+    a += b; // 使用+=运算符并更新a的值
+
+    在这个示例中，a += b 实际上是 a.operator+=(b) 的缩写，其中 *this 表示 a，并且 a 的成员数据被更新为 a 和 b 的对应成员相加的结果。
+    */
         return *this;
     }
 
@@ -79,30 +87,31 @@ using point3 = vec3;
 
 // Vector Utility Functions
 
+//@brief 重载了输出运算符，用于将 vec3 向量的内容输出到输出流
 inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
-
+//@brief 重载了加法运算符，用于执行两个 vec3 向量的加法。
 inline vec3 operator+(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
-
+//@brief 重载了减法运算符，用于执行两个 vec3 向量的减法。
 inline vec3 operator-(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
-
+//@brief 逐个相乘
 inline vec3 operator*(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
-
+//@brief 数乘
 inline vec3 operator*(double t, const vec3 &v) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
-
+//@brief 数乘
 inline vec3 operator*(const vec3 &v, double t) {
     return t * v;
 }
-
+//@brief 除法
 inline vec3 operator/(vec3 v, double t) {
     return (1/t) * v;
 }
