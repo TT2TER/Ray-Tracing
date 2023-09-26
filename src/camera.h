aa82b391f766cbc80783191b90d838c27b16fd0d
Jadeiin <github@qunn.eu>
2023-09-26 17:34:34 +0800

    update camera.h

--- src/camera.h
+++ src/camera.h
@@ -12,18 +12,18 @@
 
 class camera {
   public:
-    double aspect_ratio      = 1.0;  // Ratio of image width over height
-    int    image_width       = 100;  // Rendered image width in pixel count
-    int    samples_per_pixel = 10;   // Count of random samples for each pixel
-    int    max_depth         = 10;   // Maximum number of ray bounces into scene
+    double aspect_ratio      = 1.0;  // 图像宽高比
+    int    image_width       = 100;  // 渲染图像宽度（以像素数为单位）
+    int    samples_per_pixel = 10;   // 每个像素的随机采样数
+    int    max_depth         = 10;   // 场景中光线反射的最大次数
 
-    double vfov     = 90;              // Vertical view angle (field of view)
-    point3 lookfrom = point3(0,0,-1);  // Point camera is looking from
-    point3 lookat   = point3(0,0,0);   // Point camera is looking at
-    vec3   vup      = vec3(0,1,0);     // Camera-relative "up" direction
+    double vfov     = 90;              // 垂直视角（视野）
+    point3 lookfrom = point3(0,0,-1);  // 相机坐标
+    point3 lookat   = point3(0,0,0);   // 相机观察点坐标
+    vec3   vup      = vec3(0,1,0);     // 相机相对“向上”方向
 
-    double defocus_angle = 0;  // Variation angle of rays through each pixel
-    double focus_dist = 10;    // Distance from camera lookfrom point to plane of perfect focus
+    double defocus_angle = 0;  // 通过每个像素的光线变化角度
+    double focus_dist = 10;    // 从相机坐标到完美焦点平面的距离
 
     void render(const hittable& world) {
         initialize();
@@ -46,14 +46,14 @@ class camera {
     }
 
   private:
-    int    image_height;    // Rendered image height
-    point3 center;          // Camera center
-    point3 pixel00_loc;     // Location of pixel 0, 0
-    vec3   pixel_delta_u;   // Offset to pixel to the right
-    vec3   pixel_delta_v;   // Offset to pixel below
-    vec3   u, v, w;         // Camera frame basis vectors
-    vec3   defocus_disk_u;  // Defocus disk horizontal radius
-    vec3   defocus_disk_v;  // Defocus disk vertical radius
+    int    image_height;    // 渲染图像高度
+    point3 center;          // 相机中心
+    point3 pixel00_loc;     // 像素 0, 0 的位置
+    vec3   pixel_delta_u;   // 像素向右偏移量
+    vec3   pixel_delta_v;   // 像素向下偏移像素
+    vec3   u, v, w;         // 相机帧基向量
+    vec3   defocus_disk_u;  // 散焦盘水平半径
+    vec3   defocus_disk_v;  // 散焦盘垂直半径
 
     void initialize() {
         image_height = static_cast<int>(image_width / aspect_ratio);
@@ -61,38 +61,38 @@ class camera {
 
         center = lookfrom;
 
-        // Determine viewport dimensions.
+        // 确定视口尺寸
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta/2);
         auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
 
-        // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
+        // 计算相机坐标系的 u,v,w 单位基向量
         w = unit_vector(lookfrom - lookat);
         u = unit_vector(cross(vup, w));
         v = cross(w, u);
 
-        // Calculate the vectors across the horizontal and down the vertical viewport edges.
-        vec3 viewport_u = viewport_width * u;    // Vector across viewport horizontal edge
-        vec3 viewport_v = viewport_height * -v;  // Vector down viewport vertical edge
+        // 计算视口水平方向和垂直方向的向量
+        vec3 viewport_u = viewport_width * u;    // 视口水平方向的向量
+        vec3 viewport_v = viewport_height * -v;  // 视口垂直方向的向量
 
-        // Calculate the horizontal and vertical delta vectors to the next pixel.
+        // 计算到下一个像素的水平和垂直增量向量
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
-        // Calculate the location of the upper left pixel.
+        // 计算左上角像素的位置
         auto viewport_upper_left = center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-        // Calculate the camera defocus disk basis vectors.
+        // 计算相机散焦盘基向量
         auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
         defocus_disk_u = u * defocus_radius;
         defocus_disk_v = v * defocus_radius;
     }
 
     ray get_ray(int i, int j) const {
-        // Get a randomly-sampled camera ray for the pixel at location i,j, originating from
-        // the camera defocus disk.
+        // 获取位置 i,j 处像素的随机采样相机光线，源自
+        // 相机散焦盘。
 
         auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
         auto pixel_sample = pixel_center + pixel_sample_square();
@@ -104,26 +104,26 @@ class camera {
     }
 
     vec3 pixel_sample_square() const {
-        // Returns a random point in the square surrounding a pixel at the origin.
+        // 返回原点像素周围正方形中的随机点
         auto px = -0.5 + random_double();
         auto py = -0.5 + random_double();
         return (px * pixel_delta_u) + (py * pixel_delta_v);
     }
 
     vec3 pixel_sample_disk(double radius) const {
-        // Generate a sample from the disk of given radius around a pixel at the origin.
+        // 从原点像素周围给定半径的单位圆内采样
         auto p = radius * random_in_unit_disk();
         return (p[0] * pixel_delta_u) + (p[1] * pixel_delta_v);
     }
 
     point3 defocus_disk_sample() const {
-        // Returns a random point in the camera defocus disk.
+        // 返回相机散焦盘中的随机点
         auto p = random_in_unit_disk();
         return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
     color ray_color(const ray& r, int depth, const hittable& world) const {
-        // If we've exceeded the ray bounce limit, no more light is gathered.
+        // 如果我们超出了光线反弹限制，则不再聚集光线
         if (depth <= 0)
             return color(0,0,0);
 
